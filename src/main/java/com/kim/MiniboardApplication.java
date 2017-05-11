baeb9cdb7eb8706921de95ad0cd4a65cb70e5f69
pkgonan <pkgonan1@naver.com>
2017-05-12 00:00:41 +0900

    init

---
+++ src/main/java/com/kim/MiniboardApplication.java
@@ -0,0 +1,11 @@
+package com.kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniboardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MiniboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kim/MiniboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiniboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
